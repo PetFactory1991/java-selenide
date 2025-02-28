b31e7183ceb479bb2ba98a070d4e196d8d7f2d2b
D0rianGrey <utyfdfrthby08@gmail.com>
2025-02-28 12:43:43 +0100

    Configure Selenide to run in headless mode with Chrome browser

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -6,6 +6,8 @@ import static com.codeborne.selenide.Selenide.open;
 public class FirstTest {
     @Test
     void test() {
+        Configuration.headless = true;
+        Configuration.browser = "chrome";
         Configuration.baseUrl = "https://www.google.com";
         open("/");
     }
