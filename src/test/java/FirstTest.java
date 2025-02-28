660daa92e1916c57acd81cd333f478a0721302d1
D0rianGrey <utyfdfrthby08@gmail.com>
2025-02-28 12:51:58 +0100

    Enhance FirstTest with custom ChromeOptions for improved browser configuration

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -1,4 +1,5 @@
 import com.codeborne.selenide.Configuration;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.Test;
 
 import static com.codeborne.selenide.Selenide.open;
@@ -9,6 +10,13 @@ public class FirstTest {
         Configuration.headless = true;
         Configuration.browser = "chrome";
         Configuration.baseUrl = "https://www.google.com";
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--user-data-dir=/tmp/chrome-user-data-" + System.currentTimeMillis());
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+
+        // Применяем дополнительные возможности браузера
+        Configuration.browserCapabilities = options;
         open("/");
     }
 }
